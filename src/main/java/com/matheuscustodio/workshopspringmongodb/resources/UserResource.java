341245b1efb920bb2701ab15500e553cae0127a6
Matheus Custódio <matheuscustodio2802@gmail.com>
2023-11-03 15:37:17 -0300

    deleção de usuário

--- src/main/java/com/matheuscustodio/workshopspringmongodb/resources/UserResource.java
+++ src/main/java/com/matheuscustodio/workshopspringmongodb/resources/UserResource.java
@@ -38,7 +38,12 @@ public class UserResource {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).build();
+    }
 
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 
