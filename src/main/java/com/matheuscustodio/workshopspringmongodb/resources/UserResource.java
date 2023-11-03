ae67da5c4adcbd871cb28fb6558b509a4b3155af
Matheus Custódio <matheuscustodio2802@gmail.com>
2023-11-03 13:03:47 -0300

    implementação básica da classe user e sua resource

---
+++ src/main/java/com/matheuscustodio/workshopspringmongodb/resources/UserResource.java
@@ -0,0 +1,22 @@
+package com.matheuscustodio.workshopspringmongodb.resources;
+
+import com.matheuscustodio.workshopspringmongodb.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+    @GetMapping
+    public ResponseEntity<List<User>> findAll(){
+        User maria = new User("1", "Maria", "maria@gmail.com");
+        User alex = new User("2", "Alex", "alex@gmail.com");
+        return ResponseEntity.ok().body(new ArrayList<>(Arrays.asList(maria, alex)));
+    }
+}
