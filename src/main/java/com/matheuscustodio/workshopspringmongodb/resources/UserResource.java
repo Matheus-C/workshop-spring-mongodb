58b90376193ca670dcaba5b5ffb134e6724b23e8
Matheus Custódio <matheuscustodio2802@gmail.com>
2023-11-03 15:31:18 -0300

    Inserção de usuário

--- src/main/java/com/matheuscustodio/workshopspringmongodb/resources/UserResource.java
+++ src/main/java/com/matheuscustodio/workshopspringmongodb/resources/UserResource.java
@@ -5,11 +5,10 @@ import com.matheuscustodio.workshopspringmongodb.dto.UserDTO;
 import com.matheuscustodio.workshopspringmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -31,4 +30,16 @@ public class UserResource {
         UserDTO dto = new UserDTO(user);
         return ResponseEntity.ok().body(dto);
     }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO){
+        User obj = service.fromDTO(objDTO);
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+
+    }
+
+
 }
--- src/main/java/com/matheuscustodio/workshopspringmongodb/services/UserService.java
+++ src/main/java/com/matheuscustodio/workshopspringmongodb/services/UserService.java
@@ -1,9 +1,11 @@
 package com.matheuscustodio.workshopspringmongodb.services;
 
 import com.matheuscustodio.workshopspringmongodb.domain.User;
+import com.matheuscustodio.workshopspringmongodb.dto.UserDTO;
 import com.matheuscustodio.workshopspringmongodb.repository.UserRepository;
 import com.matheuscustodio.workshopspringmongodb.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,5 +23,13 @@ public class UserService {
     public User findById(String id){
         Optional<User> user = repo.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj){
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO obj){
+        return new User(obj.getId(), obj.getName(), obj.getEmail());
     }
 }
