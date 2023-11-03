8001d18a5f04bfcaf797f8db2d21200161bfa472
Matheus Custódio <matheuscustodio2802@gmail.com>
2023-11-03 14:18:20 -0300

    Obtenção do user por id e implementação do tratamento de erro

--- src/main/java/com/matheuscustodio/workshopspringmongodb/resources/UserResource.java
+++ src/main/java/com/matheuscustodio/workshopspringmongodb/resources/UserResource.java
@@ -1,14 +1,17 @@
 package com.matheuscustodio.workshopspringmongodb.resources;
 
 import com.matheuscustodio.workshopspringmongodb.domain.User;
+import com.matheuscustodio.workshopspringmongodb.dto.UserDTO;
 import com.matheuscustodio.workshopspringmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -16,7 +19,16 @@ public class UserResource {
     @Autowired
     private UserService service;
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
-        return ResponseEntity.ok().body(service.findAll());
+    public ResponseEntity<List<UserDTO>> findAll(){
+        List<User> list = service.findAll();
+        List<UserDTO> listDto = list.stream().map(x->new UserDTO(x)).toList();
+        return ResponseEntity.ok().body(listDto);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+        User user = service.findById(id);
+        UserDTO dto = new UserDTO(user);
+        return ResponseEntity.ok().body(dto);
     }
 }
